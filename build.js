db3785821cc590f45b086cf5889f5d5837ccf2b4
aanton <aanton@gmail.com>
2021-11-06 19:41:04 +0100

    Add development server with hot reloading

--- build.js
+++ build.js
@@ -2,15 +2,28 @@ const esbuild = require('esbuild')
 const { glob } = require('glob')
 const args = require('minimist')(process.argv.slice(2))
 
+const outDirectory = 'public'
+
 const build = (files) => {
   esbuild.build({
     entryPoints: files,
     bundle: true,
     logLevel: 'info',
     minify: args.minify === true,
-    outdir: 'public',
+    outdir: outDirectory,
     sourcemap: args.sourcemap === true,
     watch: args.watch === true
+  })
+}
+
+const serve = async function serve () {
+  console.log('Running server from: http://localhost:8080/')
+  const servor = require('servor')
+  await servor({
+    fallback: 'index.html',
+    port: 8080,
+    reload: true,
+    root: outDirectory
   })
 }
 
@@ -22,4 +35,8 @@ glob('src/*.js', (err, files) => {
 
   console.log(`Building ${files} ...`)
   build(files)
+
+  if (args.server === true) {
+    serve()
+  }
 })
